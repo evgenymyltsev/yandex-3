1213f12ab9cf081d10c356c21f656aa0194bce85
Evgeny Myltsev <myltsev.ea@gmail.com>
2018-08-30 14:44:40 +0300

    added few more tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,6 +4,7 @@ import * as _ from "lodash";
 import {Solution} from "../src/Solution";
 import {Solution_BruteForce} from "../src/Solution_BruteForce";
 import {SolutionMode} from "../src/SolutionMode";
+import * as data from "../src/Data";
 
 export function outputDataEqual(output1: Output, output2: Output): boolean {
     const epsilon = 1e-3;
@@ -88,32 +89,93 @@ describe("searching minimal consumption schedule", () => {
         jasmine.addCustomEqualityTester(outputDataEqual);
     });
 
-    it("should not greedily select lowest tariff", () => {
+    it("should find same sets of output as bruteforce does", () => {
+        const modes = [
+            SolutionMode.AllPossible,
+            SolutionMode.AllValid,
+            SolutionMode.AllMinimal,
+            SolutionMode.Production,
+        ];
+        for (const mode of modes) {
+            const answers_bruteforce = new Solution_BruteForce(data.input, mode).solve();
+            const answers = new Solution(data.input, mode).solve();
+
+            if (mode != SolutionMode.AllPossible) {
+                for (const a of answers_bruteforce) {
+                    expect(outputIsValid(data.input, a)).toBe(true);
+                }
+                for (const a of answers) {
+                    expect(outputIsValid(data.input, a)).toBe(true);
+                }
+            }
+
+            expect(answers.length).toBe(answers_bruteforce.length);
+
+            for (const answer of answers) {
+                const found = _.find(answers_bruteforce, x => outputDataEqual(x, answer));
+                expect(found).toBeDefined();
+            }
+        }
+    });
+
+    it("should not greedily select lowest tariff for single device", () => {
         const input: Input = {
             maxPower: 10,
             devices: [
-                { id: 'a', name: 'a', power: 10, duration: 4, mode: 'day', },
+                {id: 'a', name: 'a', power: 10, duration: 4, mode: 'day',},
             ],
             rates: [
                 {from: 7, to: 9, value: 10},
                 {from: 9, to: 11, value: 20},
-                {from: 11, to: 14, value: 5},
-                {from: 14, to: 21, value: 5},
+                {from: 11, to: 21, value: 5},
                 {from: 21, to: 7, value: 100},
             ]
         };
         const expectedOutput: Output = {
-            "schedule": {
-                "0": [], "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": [], "10": [],
-                "11": [ "a" ], "12": [ "a" ], "13": [ "a" ], "14": [ "a" ],
-                "15": [], "16": [], "17": [], "18": [], "19": [], "20": [], "21": [], "22": [], "23": []
+            schedule: {
+                0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [],
+                11: ["a"], 12: ["a"], 13: ["a"], 14: ["a"],
+                15: [], 16: [], 17: [], 18: [], 19: [], 20: [], 21: [], 22: [], 23: []
             },
-            "consumedEnergy": {
-                "value": 0.2,
-                "devices": { "a": 0.2 }
+            consumedEnergy: {
+                value: 0.2,
+                devices: {"a": 0.2}
             }
         };
 
+        const answer = new Solution(input).solve()[0];
+
+        expect(outputIsValid(input, answer)).toBe(true);
+        expect(outputDataEqual(answer, expectedOutput)).toBe(true);
+        expect(_.find(allMinimalBruteforce(input), x => outputDataEqual(x, answer))).toBeDefined();
+    });
+
+    it("should not greedily select lowest tariffs for two devices", () => {
+        const input: Input = {
+            maxPower: 10,
+            devices: [
+                {id: 'a', name: 'a', power: 10, duration: 4, mode: 'day',},
+                {id: 'b', name: 'b', power: 10, duration: 2, mode: 'day',},
+            ],
+            rates: [
+                {from: 7, to: 9, value: 10},
+                {from: 9, to: 11, value: 30},
+                {from: 11, to: 13, value: 5},
+                {from: 13, to: 21, value: 10},
+                {from: 21, to: 7, value: 100},
+            ]
+        };
+
+        const expectedOutput: Output = {
+            schedule: {
+                0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [],
+                11: [ 'a' ], 12: [ 'a' ], 13: [ 'a' ], 14: [ 'a' ],
+                15: [],
+                16: [ 'b' ], 17: [ 'b' ],
+                18: [], 19: [], 20: [], 21: [], 22: [], 23: []
+            },
+            consumedEnergy: { value: 0.5, devices: { a: 0.3, b: 0.2 } }
+        };
 
         const answer = new Solution(input).solve()[0];
 
@@ -123,7 +185,18 @@ describe("searching minimal consumption schedule", () => {
     });
 
     it("should return empty result when there is no solution", () => {
-        expect(true).toBe(true);
+        const input: Input = {
+            maxPower: 10,
+            devices: [
+                {id: 'a', name: 'a', power: 100, duration: 4, mode: 'day',},
+            ],
+            rates: [
+                {from: 7, to: 7, value: 10}
+            ]
+        };
+
+        const answer = new Solution(input).solve();
+        expect(answer.length).toBe(0);
     });
 
     it("should pass 'stress' test", () => {
